bbd8bd9f491b843f144c6547f9b5a34305abd144
frediricodyanes <frediricodyanes@gmail.com>
2026-05-06 09:29:45 +0300

    Ajout des projet 20

---
+++ 20_projet_pret/20_projet_a_corriger_terminer/5. AppMétéo/script.js
@@ -0,0 +1,44 @@
+const loader = document.querySelector(".loader-container");
+const errorInformation = document.querySelector(".error-information");
+
+async function getWeatherData(){
+  try {
+    const IQAIR_API_KEY = "0f31c65d-2f34-493b-895c-05b4c85d6e3a"; // Remplacez par votre clé API IQAir valide sur https://www.iqair.com/air-pollution-data-api
+    const response = await fetch(`https://api.airvisual.com/v2/nearest_city?key=${IQAIR_API_KEY}`)
+
+    if(!response.ok) {
+      throw new Error(`Error ${response.status}, ${response.statusText}`)
+    }
+
+    const responseData = await response.json();
+    
+    const sortedData = {
+      city: responseData.data.city,
+      country: responseData.data.country,
+      iconId: responseData.data.current.weather.ic,
+      temperature: responseData.data.current.weather.tp,
+    }
+
+    populateUI(sortedData)
+  }
+  catch (error) {
+    loader.classList.remove("active");
+    errorInformation.textContent = error.message;
+  }
+}
+getWeatherData()
+
+
+const cityName = document.querySelector(".city-name");
+const countryName = document.querySelector(".country-name");
+const temperature = document.querySelector(".temperature");
+const infoIcon = document.querySelector(".info-icon");
+
+function populateUI(data){
+  cityName.textContent = data.city;
+  countryName.textContent = data.country;
+  temperature.textContent = `${data.temperature}°`;
+  infoIcon.src = `ressources/icons/${data.iconId}.svg`;
+  infoIcon.style.width = "150px";
+  loader.classList.remove("active");
+}
